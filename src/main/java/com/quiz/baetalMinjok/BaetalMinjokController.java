c4e2fcba8be8e6f5600bf23b40dda4de724a5667
LeeMinHee <98minh2@gmail.com>
2024-07-06 22:34:38 +0900

    ㅇㅇ

--- src/main/java/com/quiz/baetalMinjok/BaetalMinjokController.java
+++ src/main/java/com/quiz/baetalMinjok/BaetalMinjokController.java
@@ -1,7 +1,11 @@
 package com.quiz.baetalMinjok;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/baetalMinjok")
@@ -15,4 +19,10 @@ public class BaetalMinjokController {
 		return "baetalMinjok/storeList";
 	}
 
+	// 2. 가게 리스트select
+	// http://localhost:8080/baetalMinjok/store-list
+	@PostMapping("/store-list")
+	public List<BaetalMinjok> storeList(Model model) {
+		
+	}
 }
